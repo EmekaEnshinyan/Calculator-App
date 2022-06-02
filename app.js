9280f4ac27e2b93653cd6b5d0a77e66a33f2c946
Enshinyan <emeka.enshinyan@gmail.com>
2022-06-01 20:33:18 -0700

    new: 2 num buttons and sum operator.

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-const one = document.getElementById("one").addEventListener('click', inputNum);
-const two = document.getElementById("two").addEventListener('click', inputNum);
-const addBtn = document.getElementById("addition").addEventListener('click', add);
-let arr = [one, two];
+//NEXT: find a way to allow for two button presses before the arithmetic operation goes through
+    //research Await and Promises pg 194
+
+const nums = document.getElementById("numbers");
+let numNodes = nums.getElementsByTagName("button");
+console.log("number of buttons: " + numNodes.length);
+console.log("value of first button: " + numNodes[0].innerText);
+
+
+nums.addEventListener('click', makeNums);
+//let numArray = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "zero"];
+let numArray = ["one", "two"];
 let one2 = document.getElementById('one').innerText = "1"
 //could have a function for each number
 
 function makeNums(){
+    for (let i = 0; i < numNodes.length; i++){
+        if (numNodes[i].innerText == 1){
+            let calcNum = numNodes[i].innerText;
+            console.log("output from MakeNums funct: " + calcNum);   
+            return () => calcNum;
+        }else{
+            console.log(false)
+        }
+    }
+}
+let operand = makeNums();
 
-    
-    
+function addition(){
+
 }
 
 
