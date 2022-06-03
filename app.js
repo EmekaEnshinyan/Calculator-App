209d0c2edab393e92021ddf434935b39ae99dcc3
Enshinyan <emeka.enshinyan@gmail.com>
2022-06-02 18:39:41 -0700

    edit: connected operand to operator.

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
 //NEXT: find a way to allow for two button presses before the arithmetic operation goes through
     //research Await and Promises pg 194
-let x;
-let y;
-const nums = document.getElementById("numbers");
+let nums = document.getElementById("numbers");
 let numNodes = nums.getElementsByTagName("button");
 console.log("number of buttons: " + numNodes.length);
 console.log("value of first button: " + numNodes[0].innerText);
 
 //bind value of whichever number button pressed
-let numBtnOne = nums.addEventListener('click', function callNum(event){
-    console.log(event.target.innerText);
- 
-    //return event.target.innerText;
+let result;
+    nums.addEventListener('click', function(event){
+        event.preventDefault;
+        result = getOperands(event.target.innerText); 
+    });
+
+/*be able to log the value of button pressed*/
+
+
+//output operand
+function getOperands(value){
+    console.log(value); 
+    return value;
+}
+
+/*find a way to place value(s) of button presses into an addition function*/
+function addition(operand){
+    operand.preventDefault();
+    console.log(result)
     
-});
-console.log(numBtnOne)
+
+}
 
 const addBtn = document.getElementById("addition").addEventListener('click', addition);
 
 
+
+
+
+
+
+/*extra*/
 //let numArray = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "zero"];
 //let numArray = [1, 2];
-
-function addition(event){
-    event.preventDefault();
-
-}
