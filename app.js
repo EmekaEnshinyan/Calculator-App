45fc86803c9e8e55c7bee7ca63bd37021648142c
Enshinyan <emeka.enshinyan@gmail.com>
2022-07-01 17:23:19 -0700

    misc.

--- app.js
+++ app.js
@@ -4,73 +4,17 @@ prototype:
 .this: 
 class: 
 constructors: */
-const btnOne = document.getElementById("button-one");
-const btnTwo = document.getElementById("button-two");
-const btnThree = document.getElementById("button-three");
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
 const obj = {
   w: 5,
   x: [1, 2, 3, 4, 5],
   y: true,
-  invoke: function(){
-      console.log("hello");
-  }
 }
 //returns property of obj
 console.log(obj.x);
 //modifies obj property
 console.log(obj.w = 10);
 //invokes funciton method in obj
-obj.invoke();
 
-/*
-//construct object and create the properties
- //which will be the arrays and the methods
-  //which will invoke the proper operation
-
-const o1 = document.getElementById("one");
-const o2 = document.getElementById("two");
-const o3 = document.getElementById("+");
-const o4 = document.getElementById("-");
-
-let object = {
-	operands: "",
-	opHolder: "",
-	operators: "",
-	operation: function(){
-	(!this.operand.has() === NaN)
-		return 
-	}
-	result: ""
-	
-}
-let first = object.operands = o1;
-let second = object.operands = o2;
-let operation = object.operators = o3;
-
-return `${first}${operation}${second}`
-
-
-function add(o1, o2){ //doesn't work. so
-			//instead, need to 
-			//store whatever num
-			//was pressed
-
-
-}
-
-//call each mutable part and build the chained 
- //operations
-
-//this includes the operation function
-let operation = {
-    numbers: [1,2,3,4,5],
-    add: function (x) {
-        this.numbers.forEach(x =>{
-            this.numbers;
-        })},
-     subtract: function (y) {
-        this.numbers.forEach(y =>{
-            this.numbers;
-        })},
-}
-*/
---
+++ objects.js
@@ -0,0 +1,59 @@
+
+//construct object and create the properties
+ //which will be the arrays and the methods
+  //which will invoke the proper operation
+
+const n1 = document.getElementById("one");
+n1.addEventListener('click', concat);
+const n2 = document.getElementById("two");
+n2.addEventListener('click', concat);
+const o1 = document.getElementById("+");
+const o2 = document.getElementById("-");
+
+let objOne = {
+	operands: "",
+	opHolder: "",
+	operators: "",
+	operation: function(){
+	(!this.operand.has() === NaN)
+		return 
+	},
+	result: "",
+	
+}
+let first = object.operands = n1;   
+let second = object.operands = n2;
+let operate = object.operators = o1;
+
+let arr = [1,2,3,4,5,6,7,8,9,0];
+
+
+return `${first}${operate}${second}`
+
+function concat(insert){
+    let x = ``;
+    x += `${insert}`
+    console.log(x);
+}
+
+function add(o1, o2){ //doesn't work. so
+			//instead, need to 
+			//store whatever num
+			//was pressed
+}
+
+//call each mutable part and build the chained 
+ //operations
+
+//this includes the operation function
+let objTwo = {
+    numbers: [1,2,3,4,5],
+    add: function (x) {
+        this.numbers.forEach(x =>{
+            this.numbers;
+        })},
+     subtract: function (y) {
+        this.numbers.forEach(y =>{
+            this.numbers;
+        })},
+}
