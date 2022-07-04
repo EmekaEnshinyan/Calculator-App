b27a46d5fd2ecfb76516c2083ca15fec13b219d3
Enshinyan <emeka.enshinyan@gmail.com>
2022-07-04 16:06:58 -0700

    edit: misc

--- new-attempt.js
+++ new-attempt.js
@@ -18,31 +18,54 @@ for (item of numbers){
     item.addEventListener('click', (event) =>{
         let value = event.target.innerText;
         input += value; 
-        if (math.operator != ""){
+        if (math.operator != "" && math.first != 0 && math.second == 0){
+            //i need to have the chain operation go to the second operand
             math.second = input * 1;
         }else{
             math.first = input * 1;
         }
-        console.log(math.first)
-        console.log(typeof math.first)
-        console.log(math.second)
-        console.log(typeof math.second)
+        console.log(math.first);
+        console.log(typeof math.first);
+        console.log(math.second);
+        console.log(typeof math.second);
     })
+    
+}
+
+function eval(){
+    console.log("test: ")
+    console.log(math.first + math.second)
+
 }
 
 let operators = document.getElementsByClassName("operators")
 for (item of operators){
     item.addEventListener('click', (event) => {
-        let o = event.target.innerText;
-        console.log(math.operator)
         input = "";
-        math.operator = o;
-        o = "";
+        math.operator = event.target.innerText;
+        console.log(math.operator);
         if (math.second != 0){
-            output = operations[math.operator](math.first, math.second);
+            let res = operations[math.operator](math.first, math.second);
+            console.log(res);
+            math.operator = "";
+            math.second = 0;
+            math.first = res;
+            console.log(math.first)
         }
+        
+        
     })
 }
+
+let x = 1 + 2 + 3 - 4 * 5;
+/*
+1: comes from input and put in obj as first
++: comes from input and put in obj as operator
+2: comes from input and put in obj as second
+-> evaluate -> clear properties (operator, second, first) -> set result as first
++: comes from input and put in obj as operator
+3: comes from input and put in obj as second
+-> evaluate -> clear properties (operator, second, first) -> set result as first */
 
 let math = {
     first: 0,
